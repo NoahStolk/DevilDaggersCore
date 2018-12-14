2584d6ea1846621aed41fe33eced1d8b1feba8d1
Noah Stolk <noah.stolk1997@gmail.com>
2018-12-14 14:44:27 +0100

    Add old info. Change colors for old Gigapede, transmuted skulls, old ANNIHILATED deaths. Fix Leviathan and transmuted skulls HP in V1.

--- DevilDaggersCore/Game/Game.cs
+++ DevilDaggersCore/Game/Game.cs
@@ -29,29 +29,29 @@ namespace DevilDaggersCore.Game
 			public static Death Purged = new Death("PURGED", "4E3000", 6);
 			public static Death Sacrificed = new Death("SACRIFICED", "804E00", 8);
 			public static Death Eviscerated = new Death("EVISCERATED", "837E75", 9);
-			public static Death Annihilated = new Death("ANNIHILATED", "F00000", 10);
+			public static Death Annihilated = new Death("ANNIHILATED", "BE2C20", 10);
 			public static Death Stricken = new Death("STRICKEN", "DCCB00", 16);
 			public static Death Devastated = new Death("DEVASTATED", "FF0000", 17);
 			public static Death Dismembered = new Death("DISMEMBERED", "804E00", 18);
 
-			public static Enemy Squid1 = new Enemy("Squid I", "4E3000", 10, 1, Purged, 1, 1);
-			public static Enemy Squid2 = new Enemy("Squid II", "804E00", 20, 2, Sacrificed, 2, 1);
-			public static Enemy Centipede = new Enemy("Centipede", "837E75", 75, 25, Eviscerated, 25, 25);
-			public static Enemy Gigapede = new Enemy("Gigapede", "478B41", 250, 50, Eviscerated, 50, 50); //TODO: Different color?
-			public static Enemy Leviathan = new Enemy("Leviathan", "FF0000", 1500 /*TODO: A lot less...*/, 6, Devastated, 1500, 1500);
-			public static Enemy Spider1 = new Enemy("Spider I", "097A00", 25, 1, Infested, 3, 3);
+			public static Enemy Squid1 = new Enemy("Squid I", "4E3000", 10, 1, Purged, 1, null);
+			public static Enemy Squid2 = new Enemy("Squid II", "804E00", 20, 2, Sacrificed, 2, null);
+			public static Enemy Centipede = new Enemy("Centipede", "837E75", 75, 25, Eviscerated, 25, null);
+			public static Enemy Gigapede = new Enemy("Gigapede", "7B5157", 250, 50, Eviscerated, 50, null);
+			public static Enemy Leviathan = new Enemy("Leviathan", "FF0000", 600, 6, Devastated, 600, null);
+			public static Enemy Spider1 = new Enemy("Spider I", "097A00", 25, 1, Infested, 3, null);
 
-			public static Enemy Skull1 = new Enemy("Skull I", "352710", 1, 0, Swarmed, 0.25f, 0.25f, Squid1, Squid2);
-			public static Enemy Skull2 = new Enemy("Skull II", "433114", 5, 1, Impaled, 1, 1, Squid1);
-			public static Enemy Skull3 = new Enemy("Skull III", "6E5021", 10, 1, Dismembered, 1, 1, Squid2);
+			public static Enemy Skull1 = new Enemy("Skull I", "352710", 1, 0, Swarmed, 0.25f, null, Squid1, Squid2);
+			public static Enemy Skull2 = new Enemy("Skull II", "433114", 5, 1, Impaled, 1, null, Squid1);
+			public static Enemy Skull3 = new Enemy("Skull III", "6E5021", 10, 1, Dismembered, 1, null, Squid2);
 
 			/*In V1, Leviathan turns Skull I into Transmuted Skull II, Skull II into Transmuted Skull III, Skull III into Transmuted Skull IV*/
-			public static Enemy TransmutedSkull2 = new Enemy("Transmuted Skull II", "9B0000", 20, 1/*TODO: ?*/, Impaled, 2, 2, Leviathan);
-			public static Enemy TransmutedSkull3 = new Enemy("Transmuted Skull III", "B80000", 100, 1/*TODO: ?*/, Dismembered, 10, 10, Leviathan);
-			public static Enemy TransmutedSkull4 = new Enemy("Transmuted Skull IV", "F00000", 300, 0/*TODO: ?*/, Annihilated, 30, 30, Leviathan);
+			public static Enemy TransmutedSkull2 = new Enemy("Transmuted Skull II", "721A13", 10, 1, Impaled, 1, null, Leviathan);
+			public static Enemy TransmutedSkull3 = new Enemy("Transmuted Skull III", "982319", 20, 1, Dismembered, 2, null, Leviathan);
+			public static Enemy TransmutedSkull4 = new Enemy("Transmuted Skull IV", "BE2C20", 100, 0, Annihilated, 10, null, Leviathan);
 
-			public static Enemy SpiderEgg1 = new Enemy("Spider Egg I", "99A100", 3, 0, Infested, 3, 3, Spider1);
-			public static Enemy Spiderling = new Enemy("Spiderling", "DCCB00", 3, 0, Stricken, 1, 1, SpiderEgg1);
+			public static Enemy SpiderEgg1 = new Enemy("Spider Egg I", "99A100", 3, 0, Infested, 3, null, Spider1);
+			public static Enemy Spiderling = new Enemy("Spiderling", "DCCB00", 3, 0, Stricken, 1, null, SpiderEgg1);
 		}
 
 		public static class V2
@@ -78,7 +78,7 @@ namespace DevilDaggersCore.Game
 			public static Death Desecrated = new Death("DESECRATED", "804E00", 7);
 			public static Death Sacrificed = new Death("SACRIFICED", "AF6B00", 8);
 			public static Death Eviscerated = new Death("EVISCERATED", "837E75", 9);
-			public static Death Annihilated = new Death("ANNIHILATED", "478B41", 10);
+			public static Death Annihilated = new Death("ANNIHILATED", "7B5157", 10);
 			public static Death Envenmonated = new Death("ENVENMONATED", "657A00", 12);
 			public static Death Stricken = new Death("STRICKEN", "DCCB00", 16);
 			public static Death Devastated = new Death("DEVASTATED", "FF0000", 17);
@@ -87,7 +87,7 @@ namespace DevilDaggersCore.Game
 			public static Enemy Squid2 = new Enemy("Squid II", "804E00", 20, 2, Desecrated, 2, 1);
 			public static Enemy Squid3 = new Enemy("Squid III", "AF6B00", 90, 3, Sacrificed, 3, 9);
 			public static Enemy Centipede = new Enemy("Centipede", "837E75", 75, 25, Eviscerated, 25, 25);
-			public static Enemy Gigapede = new Enemy("Gigapede", "478B41", 250, 50, Annihilated, 50, 50); //TODO: Different color?
+			public static Enemy Gigapede = new Enemy("Gigapede", "7B5157", 250, 50, Annihilated, 50, 50);
 			public static Enemy Leviathan = new Enemy("Leviathan", "FF0000", 1500, 6, Devastated, 1500, 1500);
 			public static Enemy Spider1 = new Enemy("Spider I", "097A00", 25, 1, Infested, 3, 3);
 			public static Enemy Spider2 = new Enemy("Spider II", "13FF00", 200, 1, Envenmonated, 20, 20);
@@ -97,10 +97,10 @@ namespace DevilDaggersCore.Game
 			public static Enemy Skull3 = new Enemy("Skull III", "6E5021", 10, 1, Gored, 1, 1, Squid2);
 			public static Enemy Skull4 = new Enemy("Skull IV", "976E2E", 100, 0, Opened, 10, 10, Squid3);
 
-			public static Enemy TransmutedSkull1 = new Enemy("Transmuted Skull I", "7F0000", 10, 0, Swarmed, 0.25f, 10, Leviathan);
-			public static Enemy TransmutedSkull2 = new Enemy("Transmuted Skull II", "9B0000", 20, 1, Impaled, 2, 2, Leviathan);
-			public static Enemy TransmutedSkull3 = new Enemy("Transmuted Skull III", "B80000", 100, 1, Gored, 10, 10, Leviathan);
-			public static Enemy TransmutedSkull4 = new Enemy("Transmuted Skull IV", "F00000", 300, 0, Opened, 30, 30, Leviathan);
+			public static Enemy TransmutedSkull1 = new Enemy("Transmuted Skull I", "4C110C", 10, 0, Swarmed, 0.25f, 10, Leviathan);
+			public static Enemy TransmutedSkull2 = new Enemy("Transmuted Skull II", "721A13", 20, 1, Impaled, 2, 2, Leviathan);
+			public static Enemy TransmutedSkull3 = new Enemy("Transmuted Skull III", "982319", 100, 1, Gored, 10, 10, Leviathan);
+			public static Enemy TransmutedSkull4 = new Enemy("Transmuted Skull IV", "BE2C20", 300, 0, Opened, 30, 30, Leviathan);
 
 			public static Enemy SpiderEgg1 = new Enemy("Spider Egg I", "99A100", 3, 0, Infested, 3, 3, Spider1);
 			public static Enemy SpiderEgg2 = new Enemy("Spider Egg II", "657A00", 3, 0, Envenmonated, 3, 3, Spider2);
@@ -158,10 +158,10 @@ namespace DevilDaggersCore.Game
 			public static Enemy Skull3 = new Enemy("Skull III", "6E5021", 10, 1, Gored, 1, 1, Squid2);
 			public static Enemy Skull4 = new Enemy("Skull IV", "976E2E", 100, 0, Opened, 10, 10, Squid3);
 
-			public static Enemy TransmutedSkull1 = new Enemy("Transmuted Skull I", "7F0000", 10, 0, Swarmed, 0.25f, 10, Leviathan, TheOrb);
-			public static Enemy TransmutedSkull2 = new Enemy("Transmuted Skull II", "9B0000", 20, 1, Impaled, 2, 2, Leviathan, TheOrb);
-			public static Enemy TransmutedSkull3 = new Enemy("Transmuted Skull III", "B80000", 100, 1, Gored, 10, 10, Leviathan, TheOrb);
-			public static Enemy TransmutedSkull4 = new Enemy("Transmuted Skull IV", "F00000", 300, 0, Opened, 30, 30, Leviathan, TheOrb);
+			public static Enemy TransmutedSkull1 = new Enemy("Transmuted Skull I", "4C110C", 10, 0, Swarmed, 0.25f, 10, Leviathan, TheOrb);
+			public static Enemy TransmutedSkull2 = new Enemy("Transmuted Skull II", "721A13", 20, 1, Impaled, 2, 2, Leviathan, TheOrb);
+			public static Enemy TransmutedSkull3 = new Enemy("Transmuted Skull III", "982319", 100, 1, Gored, 10, 10, Leviathan, TheOrb);
+			public static Enemy TransmutedSkull4 = new Enemy("Transmuted Skull IV", "BE2C20", 300, 0, Opened, 30, 30, Leviathan, TheOrb);
 
 			public static Enemy SpiderEgg1 = new Enemy("Spider Egg I", "99A100", 3, 0, Intoxicated, 3, 3, Spider1);
 			public static Enemy SpiderEgg2 = new Enemy("Spider Egg II", "657A00", 3, 0, Envenmonated, 3, 3, Spider2);
@@ -170,11 +170,47 @@ namespace DevilDaggersCore.Game
 
 		private static Dictionary<Enemy, string> EnemyInfo { get; set; } = new Dictionary<Enemy, string>
 		{
+			{ V1.Squid1, "Spawns at the edge of the arena\nMoves slowly and rotates clockwise\nSpawns 10 Skull Is and 1 Skull II every 20 seconds (starting 3 seconds after its initial appearance)" },
+			{ V1.Squid2, "Spawns at the edge of the arena\nMoves slowly and rotates clockwise\nSpawns 10 Skull Is and 1 Skull III every 20 seconds (starting 3 seconds after its initial appearance)" },
+			{ V1.Centipede, "Emerges and flies idly for a while, then starts chasing the player\nRegularly dives down and moves underground for a while" },
+			{ V1.Gigapede, "Emerges and flies around the arena\nRegularly dives down and moves underground for a while" },
+			{ V1.Leviathan, "Attracts and transmutes all skulls every 20 seconds\nRotates counter-clockwise\nDrops The Orb after dying" },
+			{ V1.Spider1, "Spawns at the edge of the arena and starts lifting its head, then faces the player\nAttracts and consumes gems when facing the player, ejecting them as Spider Egg I one at a time\nHides its head when shot and left nharmed for 1 second\nBegins moving randomly in an unpredictable jittery fashion after initially raising its head" },
+			{ V1.Skull1, "Slowly chases the player" },
+			{ V1.Skull2, "Moves randomly" },
+			{ V1.Skull3, "Chases the player fast" },
+			{ V1.TransmutedSkull2, "Moves randomly" },
+			{ V1.TransmutedSkull3, "Chases the player fast" },
+			{ V1.TransmutedSkull4, "Chases the player fast" },
+			{ V1.SpiderEgg1, "Hatches into 5 Spiderlings after 10 seconds" },
+			{ V1.Spiderling, "Darts towards the player in bursts with random offsets" },
+
+			{ V2.Squid1, "Spawns at the edge of the arena\nMoves slowly and rotates clockwise\nSpawns 10 Skull Is and 1 Skull II every 20 seconds (starting 3 seconds after its initial appearance)" },
+			{ V2.Squid2, "Spawns at the edge of the arena\nMoves slowly and rotates clockwise\nSpawns 10 Skull Is and 1 Skull III every 20 seconds (starting 3 seconds after its initial appearance)" },
+			{ V2.Squid3, "Spawns at the edge of the arena\nMoves slowly and rotates clockwise\nSpawns 15 Skull Is and 1 Skull IV every 20 seconds (starting 3 seconds after its initial appearance)" },
+			{ V2.Centipede, "Emerges and flies idly for a while, then starts chasing the player\nRegularly dives down and moves underground for a while" },
+			{ V2.Gigapede, "Emerges and starts chasing the player immediately" },
+			{ V2.Leviathan, "Attracts and transmutes all skulls every 20 seconds\nRotates counter-clockwise\nDrops The Orb after dying" },
+			{ V2.Spider1, "Spawns at the edge of the arena and starts lifting its head, then faces the player\nAttracts and consumes gems when facing the player, ejecting them as Spider Egg I one at a time\nHides its head when shot and left nharmed for 1 second\nBegins moving randomly in an unpredictable jittery fashion after initially raising its head" },
+			{ V2.Spider2, "Spawns at the edge of the arena and starts lifting its head (though much slower than Spider I), then faces the player\nAttracts and consumes gems when facing the player, ejecting them as Spider Egg II one at a timenHides its head when shot and left unharmed for 1 second\nBegins moving randomly in an unpredictable jittery fashion after initially raising its head (though barely noticeable due to its size)" },
+			{ V2.Skull1, "Slowly chases the player" },
+			{ V2.Skull2, "Moves randomly" },
+			{ V2.Skull3, "Chases the player fast" },
+			{ V2.Skull4, "Chases the player fast" },
+			{ V2.TransmutedSkull1, "Slowly chases the player" },
+			{ V2.TransmutedSkull2, "Moves randomly" },
+			{ V2.TransmutedSkull3, "Chases the player fast" },
+			{ V2.TransmutedSkull4, "Chases the player fast" },
+			{ V2.SpiderEgg1, "Hatches into 5 Spiderlings after 10 seconds" },
+			{ V2.SpiderEgg2, "Hatches into 5 Spiderlings after 10 seconds" },
+			{ V2.Spiderling, "Darts towards the player in bursts with random offsets" },
+			{ V2.Andras, "Unfinished enemy that was never added to the real game\nOnly appears in V2, can only be spawned using an edited spawnset\nHas its own sounds\nUses the model for Skull III, but is smaller in size\nDoes nothing but attract and consume all homing daggers like Ghostpede \nOnly takes damage when shot from above, so the player will need to daggerjump\nYou don't die when touching it" },
+
 			{ V3.Squid1, "Spawns at the edge of the arena\nMoves slowly and rotates clockwise\nSpawns 10 Skull Is and 1 Skull II every 20 seconds (starting 3 seconds after its initial appearance)" },
 			{ V3.Squid2, "Spawns at the edge of the arena\nMoves slowly and rotates clockwise\nSpawns 10 Skull Is and 1 Skull III every 20 seconds (starting 3 seconds after its initial appearance)" },
 			{ V3.Squid3, "Spawns at the edge of the arena\nMoves slowly and rotates clockwise\nSpawns 15 Skull Is and 1 Skull IV every 20 seconds (starting 3 seconds after its initial appearance)" },
 			{ V3.Centipede, "Emerges and flies idly for a while, then starts chasing the player\nRegularly dives down and moves underground for a while" },
-			{ V3.Gigapede, "Emerges and starts chasing the player immediately" }, /*TODO: Add V1 behavior and V1 and V2 image*/
+			{ V3.Gigapede, "Emerges and starts chasing the player immediately" },
 			{ V3.Ghostpede, "Emerges and starts flying in circles high above the arena\nAttracts and consumes all homing daggers, making them useless" },
 			{ V3.Leviathan, "Attracts and transmutes all skulls every 20 seconds\nRotates counter-clockwise\nDrops The Orb after dying" },
 			{ V3.Thorn, "Takes up space" },
