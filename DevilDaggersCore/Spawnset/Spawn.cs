67af7dbb23231871adaa796957fc7791a38efab0
Noah Stolk <noah.stolk1997@gmail.com>
2019-02-03 00:27:11 +0100

    Rename property.

--- DevilDaggersCore/Spawnset/Spawn.cs
+++ DevilDaggersCore/Spawnset/Spawn.cs
@@ -2,13 +2,13 @@
 {
 	public class Spawn
 	{
-		public SpawnsetEnemy Enemy { get; set; }
+		public SpawnsetEnemy SpawnsetEnemy { get; set; }
 		public double Delay { get; set; }
 		public bool IsInLoop { get; set; }
 
-		public Spawn(SpawnsetEnemy enemy, double delay, bool isInLoop = false)
+		public Spawn(SpawnsetEnemy spawnsetEnemy, double delay, bool isInLoop = false)
 		{
-			Enemy = enemy;
+			SpawnsetEnemy = spawnsetEnemy;
 			Delay = delay;
 			IsInLoop = IsInLoop;
 		}
