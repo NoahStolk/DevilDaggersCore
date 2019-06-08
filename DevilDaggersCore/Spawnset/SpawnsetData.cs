1a339db4ec066ba90e5846ccdee1283e9111f07e
Noah Stolk <noah.stolk1997@gmail.com>
2019-06-08 20:44:58 +0200

    Add serialization to SpawnsetData.

--- DevilDaggersCore/Spawnset/SpawnsetData.cs
+++ DevilDaggersCore/Spawnset/SpawnsetData.cs
@@ -1,13 +1,21 @@
-﻿namespace DevilDaggersCore.Spawnset
+﻿using Newtonsoft.Json;
+
+namespace DevilDaggersCore.Spawnset
 {
+	[JsonObject(MemberSerialization.OptIn)]
 	public class SpawnsetData
 	{
+		[JsonProperty]
 		public int NonLoopSpawns { get; set; }
+		[JsonProperty]
 		public int LoopSpawns { get; set; }
 
+		[JsonProperty]
 		public float NonLoopLength { get; set; }
+		[JsonProperty]
 		public float LoopLength { get; set; }
 
+		[JsonProperty]
 		public float LoopStart => LoopSpawns == 0 ? 0 : NonLoopLength;
 	}
 }
