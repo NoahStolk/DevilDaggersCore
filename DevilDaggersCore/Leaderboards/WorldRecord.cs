d5e628938614e957e0e29835851e551ff351e39d
Noah Stolk <noah.stolk1997@gmail.com>
2019-08-02 21:39:57 +0200

    Add WorldRecord class.

---
+++ DevilDaggersCore/Leaderboards/WorldRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DevilDaggersCore.Leaderboards
+{
+	public class WorldRecord
+	{
+		public DateTime DateTime { get; set; }
+		public Entry Entry { get; set; }
+
+		public WorldRecord(DateTime dateTime, Entry entry)
+		{
+			DateTime = dateTime;
+			Entry = entry;
+		}
+	}
+}
