d3da0f381a0924aaad0e7e016c0684531294b75a
Noah Stolk <noah.stolk1997@gmail.com>
2019-08-03 13:46:16 +0200

    Add more properties for WorldRecordHolder class.

--- DevilDaggersCore/Leaderboards/WorldRecordHolder.cs
+++ DevilDaggersCore/Leaderboards/WorldRecordHolder.cs
@@ -6,14 +6,18 @@ namespace DevilDaggersCore.Leaderboards
 	{
 		public int ID { get; set; }
 		public string Username { get; set; }
-		public TimeSpan TimeHeld { get; set; }
+		public TimeSpan TotalTimeHeld { get; set; }
+		public TimeSpan LongestTimeHeldConsecutively { get; set; }
+		public int WorldRecordCount { get; set; }
 		public DateTime LastHeld { get; set; }
 
-		public WorldRecordHolder(int id, string username, TimeSpan timeHeld, DateTime lastHeld)
+		public WorldRecordHolder(int id, string username, TimeSpan totalTimeHeld, TimeSpan longestTimeHeldConsecutively, int worldRecordCount, DateTime lastHeld)
 		{
 			ID = id;
 			Username = username;
-			TimeHeld = timeHeld;
+			TotalTimeHeld = totalTimeHeld;
+			LongestTimeHeldConsecutively = longestTimeHeldConsecutively;
+			WorldRecordCount = worldRecordCount;
 			LastHeld = lastHeld;
 		}
 	}
