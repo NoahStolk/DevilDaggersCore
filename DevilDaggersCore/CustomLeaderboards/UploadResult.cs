349548813f34362e0225142272045d2584c6b5f1
Noah Stolk <noah.stolk1997@gmail.com>
2019-08-07 20:06:47 +0200

    Move class from DDCL to here.

---
+++ DevilDaggersCore/CustomLeaderboards/UploadResult.cs
@@ -0,0 +1,16 @@
+﻿namespace DevilDaggersCore.CustomLeaderboards
+{
+	public class UploadResult
+	{
+		public bool Success { get; set; }
+		public string Message { get; set; }
+		public int TryCount { get; set; }
+
+		public UploadResult(bool success, string message, int tryCount = 0)
+		{
+			Success = success;
+			Message = message;
+			TryCount = tryCount;
+		}
+	}
+}
