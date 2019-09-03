f46af744c873b581d69d3547afeed90dc44597b2
Noah Stolk <noah.stolk1997@gmail.com>
2019-09-04 01:35:01 +0200

    Move memory handling code from DevilDaggersSurvivalEditor to here.

---
+++ DevilDaggersCore/MemoryHandling/ProcessUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace DevilDaggersCore.MemoryHandling
+{
+	public static class ProcessUtils
+	{
+		private const string ProcessNameToFind = "dd";
+		private const string ProcessMainWindowTitle = "Devil Daggers";
+
+		public static Process GetDevilDaggersProcess()
+		{
+			foreach (Process process in Process.GetProcessesByName(ProcessNameToFind))
+				if (process.MainWindowTitle == ProcessMainWindowTitle)
+					return process;
+			return null;
+		}
+	}
+}
