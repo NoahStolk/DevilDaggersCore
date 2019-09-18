10907bae4db8af3526070b4f2201cc56238ab4be
Noah Stolk <noah.stolk1997@gmail.com>
2019-09-18 16:50:18 +0200

    Remove redundant properties.

--- DevilDaggersCore/Spawnsets/SpawnsetData.cs
+++ DevilDaggersCore/Spawnsets/SpawnsetData.cs
@@ -14,13 +14,5 @@ namespace DevilDaggersCore.Spawnsets
 		public float? NonLoopLengthNullable { get; set; }
 		[JsonProperty]
 		public float? LoopLengthNullable { get; set; }
-
-		// TODO: Remove these 3 properties (will break DDSE versions 2.0.0.0 to 2.4.0.0)
-		[JsonProperty]
-		public float NonLoopLength => NonLoopLengthNullable ?? 0;
-		[JsonProperty]
-		public float LoopLength => LoopLengthNullable ?? 0;
-		[JsonProperty]
-		public float LoopStart => NonLoopLength;
 	}
 }
