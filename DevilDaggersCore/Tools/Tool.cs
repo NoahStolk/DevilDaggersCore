cba9f2d9ec57a8d9c7bc6386cb54cdfb975a12ea
Noah Stolk <noah.stolk1997@gmail.com>
2019-10-19 14:14:59 +0200

    no message

--- DevilDaggersCore/Tools/Tool.cs
+++ DevilDaggersCore/Tools/Tool.cs
@@ -7,8 +7,13 @@ namespace DevilDaggersCore.Tools
 	{
 		[JsonProperty]
 		public string Name { get; set; }
+		
 		[JsonProperty]
 		public string VersionNumber { get; set; }
+
+		/// <summary>
+		/// Indicates the oldest version of the tool which is still fully compatible with the website.
+		/// </summary>
 		[JsonProperty]
 		public string VersionNumberRequired { get; set; }
 	}
