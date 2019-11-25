79820f296162af9f1ef43a7a061a9e5faaf7a712
Noah Stolk <noah.stolk1997@gmail.com>
2019-11-25 19:48:47 +0100

    Implement multiple usernames (aliases) for WorldRecordHolder class.

--- DevilDaggersCore/Leaderboards/History/WorldRecordHolder.cs
+++ DevilDaggersCore/Leaderboards/History/WorldRecordHolder.cs
@@ -1,24 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DevilDaggersCore.Leaderboards.History
 {
 	public class WorldRecordHolder
 	{
 		public int ID { get; set; }
-		public string Username { get; set; }
+		public List<string> Usernames { get; } = new List<string>();
 		public TimeSpan TotalTimeHeld { get; set; }
 		public TimeSpan LongestTimeHeldConsecutively { get; set; }
 		public int WorldRecordCount { get; set; }
 		public DateTime LastHeld { get; set; }
 
+		public string MostRecentUsername { get; set; }
+
 		public WorldRecordHolder(int id, string username, TimeSpan totalTimeHeld, TimeSpan longestTimeHeldConsecutively, int worldRecordCount, DateTime lastHeld)
 		{
 			ID = id;
-			Username = username;
+			Usernames.Add(username);
 			TotalTimeHeld = totalTimeHeld;
 			LongestTimeHeldConsecutively = longestTimeHeldConsecutively;
 			WorldRecordCount = worldRecordCount;
 			LastHeld = lastHeld;
+
+			MostRecentUsername = username;
 		}
 	}
 }
