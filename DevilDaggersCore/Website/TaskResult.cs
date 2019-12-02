ddc7edcf8a5a1b8e37eb6172d2e75b723846e083
Noah Stolk <noah.stolk1997@gmail.com>
2019-12-02 20:24:35 +0100

    Add WebStatsResult and TaskResult classes.

---
+++ DevilDaggersCore/Website/TaskResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DevilDaggersCore.Website
+{
+	public class TaskResult
+	{
+		public string TypeName { get; private set; }
+		public DateTime LastTriggered { get; private set; }
+		public DateTime LastFinished { get; private set; }
+		public TimeSpan ExecutionTime { get; private set; }
+		public string Schedule { get; private set; }
+
+		public TaskResult(string typeName, DateTime lastTriggered, DateTime lastFinished, TimeSpan executionTime, string schedule)
+		{
+			TypeName = typeName;
+			LastTriggered = lastTriggered;
+			LastFinished = lastFinished;
+			ExecutionTime = executionTime;
+			Schedule = schedule;
+		}
+	}
+}
---
+++ DevilDaggersCore/Website/WebStatsResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DevilDaggersCore.Website
+{
+	public class WebStatsResult
+	{
+		public DateTime WebsiteBuildDateTime { get; set; }
+		public List<TaskResult> TaskResults { get; set; }
+
+		public WebStatsResult(DateTime buildDateTime, List<TaskResult> taskResults)
+		{
+			WebsiteBuildDateTime = buildDateTime;
+			TaskResults = taskResults;
+		}
+	}
+}
