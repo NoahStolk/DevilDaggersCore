96b89c720f98588ea128cf9736c881edbee663b4
Noah Stolk <noah.stolk1997@gmail.com>
2020-04-11 20:30:42 +0200

    Add SubmissionInfo class for custom leaderboards upload result

---
+++ DevilDaggersCore/CustomLeaderboards/SubmissionInfo.cs
@@ -0,0 +1,40 @@
+﻿namespace DevilDaggersCore.CustomLeaderboards
+{
+	public class SubmissionInfo
+	{
+		public int TotalPlayers { get; set; }
+
+		public int Rank { get; set; }
+		public int RankDiff { get; set; }
+
+		public float Time { get; set; }
+		public float TimeDiff { get; set; }
+
+		public int Kills { get; set; }
+		public int KillsDiff { get; set; }
+
+		public int Gems { get; set; }
+		public int GemsDiff { get; set; }
+
+		public int ShotsHit { get; set; }
+		public int ShotsHitDiff { get; set; }
+
+		public int ShotsFired { get; set; }
+		public int ShotsFiredDiff { get; set; }
+
+		public int EnemiesAlive { get; set; }
+		public int EnemiesAliveDiff { get; set; }
+
+		public int Homing { get; set; }
+		public int HomingDiff { get; set; }
+
+		public float LevelUpTime2 { get; set; }
+		public float LevelUpTime2Diff { get; set; }
+
+		public float LevelUpTime3 { get; set; }
+		public float LevelUpTime3Diff { get; set; }
+
+		public float LevelUpTime4 { get; set; }
+		public float LevelUpTime4Diff { get; set; }
+	}
+}
--- DevilDaggersCore/CustomLeaderboards/UploadResult.cs
+++ DevilDaggersCore/CustomLeaderboards/UploadResult.cs
@@ -5,12 +5,14 @@
 		public bool Success { get; set; }
 		public string Message { get; set; }
 		public int TryCount { get; set; }
+		public SubmissionInfo SubmissionInfo { get; set; }
 
-		public UploadResult(bool success, string message, int tryCount = 0)
+		public UploadResult(bool success, string message, int tryCount = 0, SubmissionInfo submissionInfo = null)
 		{
 			Success = success;
 			Message = message;
 			TryCount = tryCount;
+			SubmissionInfo = submissionInfo;
 		}
 	}
 }
