50c1eb94d48892562c7b13afb71b334bd4a31a3e
Noah Stolk <noah.stolk1997@gmail.com>
2020-04-29 21:45:27 +0200

    Implement FormatUtils class

---
+++ DevilDaggersCore/FormatUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace DevilDaggersCore
+{
+	public static class FormatUtils
+	{
+		public static readonly string LeaderboardTimeFormat = "0.0000";
+		public static readonly string LeaderboardTimeTotalFormat = "###,###,###,##0.0000";
+		public static readonly string LeaderboardIntFormat = "N0";
+		public static readonly string DateFormat = "yyyy-MM-dd";
+		public static readonly string SpawnTimeFormat = "0.0000";
+		public static readonly string MouseSensitivityFormat = "0.000";
+		public static readonly string AccuracyFormat = "0.00%";
+	}
+}
