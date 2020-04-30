919326c6a57e54b713494320a1c0c44ffd764699
Noah Stolk <noah.stolk1997@gmail.com>
2020-04-30 17:20:48 +0200

    Improve naming

--- DevilDaggersCore/FormatUtils.cs
+++ DevilDaggersCore/FormatUtils.cs
@@ -3,7 +3,7 @@
 	public static class FormatUtils
 	{
 		public static readonly string LeaderboardTimeFormat = "0.0000";
-		public static readonly string LeaderboardTimeTotalFormat = "###,###,###,##0.0000";
+		public static readonly string LeaderboardTimeLargeFormat = "###,###,###,##0.0000";
 		public static readonly string LeaderboardIntFormat = "N0";
 		public static readonly string DateFormat = "yyyy-MM-dd";
 		public static readonly string SpawnTimeFormat = "0.0000";
