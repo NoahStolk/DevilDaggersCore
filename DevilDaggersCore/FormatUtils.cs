724070d627570fe892afca5597f12716e78ebba9
Noah Stolk <noah.stolk1997@gmail.com>
2020-05-10 21:29:31 +0200

    Add includeThousandSeparator optional parameter to FormatTimeInteger method and add LeaderboardIntAverageFormat field for extra leaderboard stats

--- DevilDaggersCore/FormatUtils.cs
+++ DevilDaggersCore/FormatUtils.cs
@@ -8,17 +8,25 @@ namespace DevilDaggersCore
 		public static readonly string LeaderboardTimeFormat = "0.0000";
 		public static readonly string LeaderboardTimeLargeFormat = "###,###,###,##0.0000";
 		public static readonly string LeaderboardIntFormat = "N0";
+		public static readonly string LeaderboardIntAverageFormat = "###,###,###,##0.00";
 		public static readonly string DateFormat = "yyyy-MM-dd";
 		public static readonly string SpawnTimeFormat = "0.0000";
 		public static readonly string MouseSensitivityFormat = "0.000";
 		public static readonly string AccuracyFormat = "0.00%";
 
 		// TODO: Improve performance.
-		public static string FormatTimeInteger(this int time)
+		public static string FormatTimeInteger<T>(this T time, bool includeThousandSeparator = false)
+			where T : struct // C# does not have a type constraint for integer types.
 		{
 			string timeStr = time.ToString();
 			timeStr = $"{new string(' ', Math.Max(0, 5 - timeStr.Length))}{timeStr}";
-			return timeStr.Reverse().Insert(4, ".").Reverse();
+			timeStr = timeStr.Reverse().Insert(4, ".");
+
+			if (includeThousandSeparator)
+				for (int i = 8; i < timeStr.Length; i += 4)
+					timeStr = timeStr.Insert(i, ",");
+
+			return timeStr.Reverse();
 		}
 	}
 }
