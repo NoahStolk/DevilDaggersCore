456d30b6428bc075968140477d1e9c69ccee13d2
Noah Stolk <noah.stolk1997@gmail.com>
2020-07-17 23:18:47 +0200

    Add missing utilities and extensions

---
+++ DevilDaggersCore/Extensions/EnumExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DevilDaggersCore.Extensions
+{
+	public static class EnumExtensions
+	{
+		public static bool HasFlagBothWays<TEnum>(this TEnum a, TEnum b)
+			where TEnum : Enum
+			=> a.HasFlag(b) || b.HasFlag(a);
+	}
+}
--- DevilDaggersCore/Extensions/StringExtensions.cs
+++ DevilDaggersCore/Extensions/StringExtensions.cs
@@ -4,6 +4,14 @@ namespace DevilDaggersCore.Extensions
 {
 	public static class StringExtensions
 	{
+		public static string TrimEnd(this string s, string value)
+		{
+			if (!s.EndsWith(value))
+				return s;
+
+			return s.Remove(s.LastIndexOf(value));
+		}
+
 		public static string Reverse(this string s)
 		{
 			char[] charArray = s.ToCharArray();
