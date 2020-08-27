69625254c0af2722e0afeaca752e9d7abbfd62c4
Noah Stolk <noah.stolk1997@gmail.com>
2020-08-27 19:55:03 +0200

    Remove MathUtils.Clamp now that it is present in .NET Core

--- DevilDaggersCore/Utils/MathUtils.cs
+++ DevilDaggersCore/Utils/MathUtils.cs
@@ -1,14 +1,8 @@
-﻿using System;
-
-namespace DevilDaggersCore.Utils
+﻿namespace DevilDaggersCore.Utils
 {
 	public static class MathUtils
 	{
 		public static float Lerp(float value1, float value2, float amount)
 			=> value1 + (value2 - value1) * amount;
-
-		public static T Clamp<T>(T value, T min, T max)
-			where T : IComparable<T>
-			=> (value.CompareTo(min) < 0) ? min : (value.CompareTo(max) > 0) ? max : value;
 	}
 }
