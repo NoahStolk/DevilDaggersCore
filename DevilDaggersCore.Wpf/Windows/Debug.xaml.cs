d608a1af77ceacdb778781da056807d4039386ed
Noah Stolk <noah.stolk1997@gmail.com>
2020-09-09 21:58:50 +0200

    Add new DebugWindow

---
+++ DevilDaggersCore.Wpf/Windows/Debug.xaml.cs
@@ -0,0 +1,28 @@
+﻿using DevilDaggersCore.Wpf.Utils;
+using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DevilDaggersCore.Wpf.Windows
+{
+	public partial class DebugWindow : Window
+	{
+		public DebugWindow()
+		{
+			InitializeComponent();
+
+			LabelSuccess.Foreground = new SolidColorBrush(ColorUtils.ColorSuccess);
+			LabelSuggestion.Foreground = new SolidColorBrush(ColorUtils.ColorSuggestion);
+			LabelWarning.Foreground = new SolidColorBrush(ColorUtils.ColorWarning);
+			LabelError.Foreground = new SolidColorBrush(ColorUtils.ColorError);
+
+			TestExceptionButton.Click += (sender, e) => throw new Exception("Test exception");
+
+			ShowMessageButton.Click += (sender, e) =>
+			{
+				MessageWindow messageWindow = new MessageWindow("Test title", "Test message");
+				messageWindow.ShowDialog();
+			};
+		}
+	}
+}
