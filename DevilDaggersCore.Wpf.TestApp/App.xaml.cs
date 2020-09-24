e9aae059668c4531cd18c739f8e5f570c599d42c
Noah Stolk <noah.stolk1997@gmail.com>
2020-09-24 19:12:55 +0200

    Add DevilDaggersCore.Wpf.TestApp project to test GUI

---
+++ DevilDaggersCore.Wpf.TestApp/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace DevilDaggersCore.Wpf.TestApp
+{
+	public partial class App : Application
+	{
+	}
+}
---
+++ DevilDaggersCore.Wpf.TestApp/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DevilDaggersCore.Wpf.Windows;
+using System.Windows;
+
+namespace DevilDaggersCore.Wpf.TestApp
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			DebugWindow dw = new DebugWindow();
+			dw.ShowDialog();
+		}
+	}
+}
