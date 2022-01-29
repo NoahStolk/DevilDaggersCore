f89e640827ab201f1f301afd165b3b3a1b40c058
Noah Stolk <noah.stolk@gmail.com>
2022-01-29 19:38:32 +0100

    Remove all DDSE functionality

--- DevilDaggersCore/Spawnsets/GameMode.cs
+++
@@ -1,7 +0,0 @@
-namespace DevilDaggersCore.Spawnsets;
-
-public enum GameMode
-{
-	Default = 0,
-	TimeAttack = 1,
-}
--- DevilDaggersCore/Spawnsets/SpawnsetData.cs
+++
@@ -1,3 +0,0 @@
-namespace DevilDaggersCore.Spawnsets;
-
-public record SpawnsetData(int SpawnVersion, int WorldVersion, GameMode GameMode, int NonLoopSpawnCount, int LoopSpawnCount, float? NonLoopLength, float? LoopLength, byte? Hand, int? AdditionalGems, float? TimerStart);
