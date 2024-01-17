fa83fd7f120d53b4c63492d84036335eeab92de0
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-01-17 16:01:23 +0100

    Remove deprecated windows and release 4.0.0

--- DevilDaggersCore.Wpf/Windows/Debug.xaml.cs
+++ DevilDaggersCore.Wpf/Windows/Debug.xaml.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace DevilDaggersCore.Wpf.Windows;
@@ -10,28 +8,15 @@ public partial class DebugWindow : Window
 	{
 		InitializeComponent();
 
-		TestExceptionButton.Click += (sender, e) => throw new("Test exception");
+		TestExceptionButton.Click += (_, _) => throw new("Test exception");
 
-		ShowChangelogButton.Click += (sender, e) => ShowWindow(new ChangelogWindow(new() { new(new(1, 0, 0, 0), DateTime.Now, new() { new("Initial release", new() { new("Test", null) }) }) }, new(1, 0, 0, 0)));
+		ShowConfirmButton.Click += (_, _) => ShowWindow(new ConfirmWindow("Test confirm", "Confirm?", true));
 
-		ShowCheckingForUpdatesButton.Click += (sender, e) => ShowWindow(new CheckingForUpdatesWindow(SimulateCheckingForUpdates));
+		ShowErrorButton.Click += (_, _) => ShowWindow(new ErrorWindow("Test error", "Test error", null));
 
-		ShowConfirmButton.Click += (sender, e) => ShowWindow(new ConfirmWindow("Test confirm", "Confirm?", true));
-
-		ShowErrorButton.Click += (sender, e) => ShowWindow(new ErrorWindow("Test error", "Test error", null));
-
-		ShowMessageButton.Click += (sender, e) => ShowWindow(new MessageWindow("Test title", "Test message"));
-
-		ShowUpdateRecommendedButton.Click += (sender, e) => ShowWindow(new UpdateRecommendedWindow("1.0.0.1", "1.0.0.0", "TestApp", "Test App"));
+		ShowMessageButton.Click += (_, _) => ShowWindow(new MessageWindow("Test title", "Test message"));
 	}
 
 	private static void ShowWindow(Window window)
 		=> window.ShowDialog();
-
-	private static bool SimulateCheckingForUpdates()
-	{
-		Task.Delay(1000).Wait();
-
-		return true;
-	}
 }
